ec5f14d00f4b2c84b18e062ace9f5eeb285f7571
aliazhar <aliazhar.idx@gmail.com>
2024-01-23 20:50:47 +0700

    [ +^ ] Integrate the banner with API which contains trending movies, also add the functionality

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -3,11 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class MovieController extends Controller
 {
   public function index()
   {
-    return view('home');
+    // Max movie trends to show
+    $MAX_TREND = 7;
+    $trending = [];
+
+    $baseURL = env('MOVIE_DB_BASE_URL');
+    $imageBaseURL = env('MOVIE_DB_IMAGE_BASE_URL');
+    $apiKey = env('MOVIE_DB_API_KEY');
+
+    // Do request api to get trends movie
+    $trendingResponse = Http::get("{$baseURL}/trending/movie/week", [
+      'api_key' => $apiKey,
+    ]);
+
+    if ($trendingResponse->successful()) {
+      $result = $trendingResponse->object()->results;
+
+      if (isset($result)) $trending = array_slice($result, 0, $MAX_TREND);
+    }
+
+    return view('home', [
+      'baseURL' => $baseURL,
+      'imageBaseURL' => $imageBaseURL,
+      'apiKey' => $apiKey,
+      'trending' => $trending
+    ]);
   }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,33 +19,37 @@
     {{-- Banner Section --}}
     <div class="w-full h-[512px] flex flex-col relative bg-black">
       {{-- Banner Data --}}
-      <div class="flex flex-row items-center w-full h-full relative slide">
-        {{-- Image --}}
-        <img src="https://via.placeholder.com/960x512" alt="" class="absolute w-full h-full object-cover">
+      @foreach ($trending as $movie)
+        @php
+          $movieImageURL = "{$imageBaseURL}/original/{$movie->backdrop_path}";
+        @endphp
 
-        {{-- Image Overlay --}}
-        <div class="w-full h-full absolute bg-black bg-opacity-40"></div>
+        <div class="flex flex-row items-center w-full h-full relative slide duration-200">
+          {{-- Image --}}
+          <img src="{{ $movieImageURL }}" alt="{{ $movie->title }}" class="absolute w-full h-full object-cover">
 
-        <div class="w-10/12 flex flex-col col ml-28 z-10 ">
-          <span class="font-bold font-inter text-4xl text-white">Title</span>
-          <span class="font-inter text-xl text-white w-1/2 line-clamp-2">Lorem ipsum dolor, sit amet consectetur
-            adipisicing elit. Culpa, voluptatibus optio dolorum ratione est iure consectetur rerum quam in modi quasi
-            esse delectus atque aperiam vero velit obcaecati mollitia deserunt?</span>
-          <a href="/movie/id"
-            class="w-fit bg-movieapp-500 text-white pl-4 pr-4 py-2 mt-5 font-inter text-sm flex flex-row rounded-full items-center hover:drop-shadow-lg duration-200">
-            <svg width="11" height="14" viewBox="0 0 11 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M0.5 12.175V1.825C0.5 1.54167 0.6 1.30417 0.8 1.1125C1 0.920833 1.23333 0.825 1.5 0.825C1.58333 0.825 1.67083 0.8375 1.7625 0.8625C1.85417 0.8875 1.94167 0.925 2.025 0.975L10.175 6.15C10.325 6.25 10.4375 6.375 10.5125 6.525C10.5875 6.675 10.625 6.83333 10.625 7C10.625 7.16667 10.5875 7.325 10.5125 7.475C10.4375 7.625 10.325 7.75 10.175 7.85L2.025 13.025C1.94167 13.075 1.85417 13.1125 1.7625 13.1375C1.67083 13.1625 1.58333 13.175 1.5 13.175C1.23333 13.175 1 13.0792 0.8 12.8875C0.6 12.6958 0.5 12.4583 0.5 12.175Z"
-                fill="#FFF" />
-            </svg>
-            <span class="pl-1">Detail</span>
-          </a>
+          {{-- Image Overlay --}}
+          <div class="w-full h-full absolute bg-black bg-opacity-40"></div>
+
+          <div class="w-10/12 flex flex-col col ml-28 z-10 ">
+            <span class="font-bold font-inter text-4xl text-white">{{ $movie->title }}</span>
+            <span class="font-inter text-xl text-white w-1/2 line-clamp-2">{{ $movie->overview }}</span>
+            <a href="/movie/{{ $movie->id }}"
+              class="w-fit bg-movieapp-500 text-white pl-4 pr-4 py-2 mt-5 font-inter text-sm flex flex-row rounded-full items-center hover:drop-shadow-lg duration-200">
+              <svg width="11" height="14" viewBox="0 0 11 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M0.5 12.175V1.825C0.5 1.54167 0.6 1.30417 0.8 1.1125C1 0.920833 1.23333 0.825 1.5 0.825C1.58333 0.825 1.67083 0.8375 1.7625 0.8625C1.85417 0.8875 1.94167 0.925 2.025 0.975L10.175 6.15C10.325 6.25 10.4375 6.375 10.5125 6.525C10.5875 6.675 10.625 6.83333 10.625 7C10.625 7.16667 10.5875 7.325 10.5125 7.475C10.4375 7.625 10.325 7.75 10.175 7.85L2.025 13.025C1.94167 13.075 1.85417 13.1125 1.7625 13.1375C1.67083 13.1625 1.58333 13.175 1.5 13.175C1.23333 13.175 1 13.0792 0.8 12.8875C0.6 12.6958 0.5 12.4583 0.5 12.175Z"
+                  fill="#FFF" />
+              </svg>
+              <span class="pl-1">Detail</span>
+            </a>
+          </div>
         </div>
-      </div>
+      @endforeach
 
       {{-- Prev Button --}}
       <div class="absolute left-0 top-1/2 -translate-y-1/2 w-1/12 flex justify-center">
-        <button class="bg-white p-3 rounded-full opacity-20 hover:opacity-100 duration-200">
+        <button onclick="moveSlide(-1)" class="bg-white p-3 rounded-full opacity-20 hover:opacity-100 duration-200">
           <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
             <mask id="mask0_203_62" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="25"
               height="25">
@@ -63,7 +67,7 @@
 
       {{-- Next Button --}}
       <div class="rotate-180 absolute right-0 top-1/2 -translate-y-1/2 w-1/12 flex justify-center">
-        <button class="bg-white p-3 rounded-full opacity-20 hover:opacity-100 duration-200">
+        <button onclick="moveSlide(1)" class="bg-white p-3 rounded-full opacity-20 hover:opacity-100 duration-200">
           <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
             <mask id="mask0_203_62" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="25"
               height="25">
@@ -82,14 +86,62 @@
       {{-- Indicator --}}
       <div class="absolute bottom-0 w-full mb-8">
         <div class="w-full flex flex-row items-center justify-center">
-          <div class="w-2.5 h-2.5 rounded-full mx-1 cursor-pointer bg-white dot"></div>
-          <div class="w-2.5 h-2.5 rounded-full mx-1 cursor-pointer bg-white dot"></div>
-          <div class="w-2.5 h-2.5 rounded-full mx-1 cursor-pointer bg-white dot"></div>
-          <div class="w-2.5 h-2.5 rounded-full mx-1 cursor-pointer bg-movieapp-500 dot"></div>
-          <div class="w-2.5 h-2.5 rounded-full mx-1 cursor-pointer bg-white dot"></div>
+          @for ($pos = 1; $pos <= count($trending); $pos++)
+            <div class="w-2.5 h-2.5 rounded-full mx-1 cursor-pointer dot" onclick="currentSlide($pos)"></div>
+          @endfor
         </div>
       </div>
     </div>
+
+    <script>
+      // Default active slide
+      let slideIndex = 1;
+      showSlide(slideIndex);
+
+      function showSlide(position) {
+        const slides = document.getElementsByClassName('slide');
+        const dots = document.getElementsByClassName('dot');
+
+        // Looping effect
+        if (position > slides.length) {
+          slideIndex = 1;
+        }
+
+        if (position < 1) {
+          slideIndex = slides.length
+        }
+
+        // Hide all slides
+        for (let i = 0; i < slides.length; i++) {
+          slides[i].classList.add('hidden');
+        }
+
+        // Show active slide
+        slides[slideIndex - 1].classList.remove('hidden');
+
+        // Remove active status
+        for (let i = 0; i < dots.length; i++) {
+          dots[i].classList.remove('bg-movieapp-500');
+          dots[i].classList.add('bg-white');
+        }
+
+        // Set active status
+        dots[slideIndex - 1].classList.remove('bg-white');
+        dots[slideIndex - 1].classList.add('bg-movieapp-500');
+      }
+
+      function moveSlide(moveStep) {
+        showSlide(slideIndex += moveStep);
+      }
+
+      function currentSlide(position) {
+        showSlide(slideIndex = position);
+      }
+
+      setInterval(() => {
+        showSlide(slideIndex += 1)
+      }, 10000);
+    </script>
 </body>
 
 </html>
