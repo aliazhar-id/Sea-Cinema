5e6033518eeec08fe851b8c7775d5e016d63812e
aliazhar <aliazhar.idx@gmail.com>
2024-01-23 10:29:29 +0700

    [ + ] Add Home page; controller; partials header

---
+++ app/Http/Controllers/MovieController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MovieController extends Controller
+{
+  public function index()
+  {
+    return view('home');
+  }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Movies App</title>
+
+  @vite('resources/css/app.css')
+</head>
+
+<body>
+  <div class="w-full h-auto min-h-screen flex flex-col">
+
+    {{-- Header Section --}}
+    @include('partials.header')
+
+  </div>
+</body>
+
+</html>
