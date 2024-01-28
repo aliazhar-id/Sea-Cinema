76dfc5a6e764348dd779eb11f151cdd796e92bc1
aliazhar <aliazhar.idx@gmail.com>
2024-01-28 14:24:18 +0700

    [ - ] Remove tv shows pages; etc

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,9 +2,6 @@
   <div class="w-1/3 pl-5">
     <a href="/movies"
       class="uppercase text-base mx-5 text-black hover:text-movieapp-500 duration-200 font-inter">Movies</a>
-    <a href="/tv-shows" class="uppercase text-base mx-5 text-black hover:text-movieapp-500 duration-200 font-inter">
-      TV Shows
-    </a>
   </div>
 
   <div class="w-1/3 flex items-center justify-center">
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [MovieController::class, 'index']);
 Route::get('/movies', [MovieController::class, 'movies']);
-Route::get('/tv-shows', [MovieController::class, 'tvShows']);
 Route::get('/search', [MovieController::class, 'search']);
 Route::get('/movie/{id}', [MovieController::class, 'movieDetails']);
-Route::get('/tv/{id}', [MovieController::class, 'tvDetails']);
