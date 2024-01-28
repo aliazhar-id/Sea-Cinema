b5a0e5df81a3e4faf4fba15b693674c4802c993a
aliazhar <aliazhar.idx@gmail.com>
2024-01-28 14:35:06 +0700

    [ ^ ] Rebrand: Movies App to Sea Cinema

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -6,7 +6,7 @@
 
   <div class="w-1/3 flex items-center justify-center">
     <a href="/" class="font-bold text-4xl font-quicksand text-black hover:text-movieapp-500 duration-200">
-      MOVIES APP
+      SEA CINEMA
     </a>
   </div>
   <div class="w-1/3 flex flex-row justify-end pr-10">
